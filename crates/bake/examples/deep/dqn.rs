f5fcf2d7cacd6c0d054f9518372ef38144bd01ee
joonjoon0425 <joonjoon0425@gmail.com>
2026-08-17 03:04:46 +0900

    [bake][directory] Separated tabular and deep examples. - Added dueling_dqn example

--- crates/bake/examples/deep/dqn.rs
+++ crates/bake/examples/deep/dqn.rs
@@ -1,4 +1,4 @@
-use bake_deep::{agent::DQNAgent, buffer::ReplayBuffer, encoder::MLPEncoder, env::CartPole, head::{DuelingLinearQHead, LinearQHead}, policy::EpsGreedy, qnetwork::SequentialQNetwork, types::{Tape, Transition}};
+use bake_deep::{agent::DQNAgent, buffer::ReplayBuffer, encoder::MLPEncoder, env::CartPole, head::LinearQHead, policy::EpsGreedy, qnetwork::SequentialQNetwork, types::Tape};
 use burn::{Tensor, nn::{Relu}, optim::AdamConfig, tensor::Device};
 use burn::nn::activation::Activation;
 pub fn main() {
@@ -8,7 +8,7 @@ pub fn main() {
     let mut agent = DQNAgent::new(0.99,    
         SequentialQNetwork::new(
             MLPEncoder::new(vec![4, 128], Activation::Relu(Relu), &device),
-            DuelingLinearQHead::new(128, 2, &device)
+            LinearQHead::new(128, 2, &device)
         ),
         1e-3,
             AdamConfig::new().init()
---
+++ crates/bake/examples/deep/dueling_dqn.rs
@@ -0,0 +1,46 @@
+use bake_deep::{agent::DQNAgent, buffer::ReplayBuffer, encoder::MLPEncoder, env::CartPole, head::DuelingLinearQHead, policy::EpsGreedy, qnetwork::SequentialQNetwork, types::Tape};
+use burn::{Tensor, nn::{Relu}, optim::AdamConfig, tensor::Device};
+use burn::nn::activation::Activation;
+pub fn main() {
+    let device = Device::default().autodiff();
+    device.seed(12);
+    let mut env = CartPole::new(12, &device);
+    let mut agent = DQNAgent::new(0.99,    
+        SequentialQNetwork::new(
+            MLPEncoder::new(vec![4, 128], Activation::Relu(Relu), &device),
+            DuelingLinearQHead::new(128, 2, &device)
+        ),
+        1e-3,
+            AdamConfig::new().init()
+        );
+    let mut policy = EpsGreedy::new(123, 1.0f32);
+    let mut buffer = ReplayBuffer::new(12, 10000, device.clone());
+    let mut tape = Tape::new(&mut env);
+    let mut count = 0;
+    for episode in 0..=4000 {
+        let mut steps = 0;
+        let mut loss: Tensor<1> = Tensor::zeros([1], &device);
+        let mut td_error: Tensor<1> = Tensor::zeros([1], &device);
+        let mut q_mean: Tensor<1> = Tensor::zeros([1], &device);
+        tape.reset(&mut env);
+        loop {
+            let action = agent.action(&mut policy, tape.obs.clone(), tape.mask.clone());
+            let t = tape.step(&mut env, action);
+            let done = t.terminated || t.truncated;
+            buffer.push(t);
+
+            if let Some(batch) = buffer.sample(64) {
+                (agent, q_mean, loss, td_error) = agent.update(batch);
+            }
+
+            if count % 1000 == 0 {
+                agent.sync();
+            }
+            count += 1;
+            steps += 1;
+            if done { break; }
+        }
+        *policy.eps_mut() *= 0.99;
+        if episode % 100 == 0 { println!("episode: {episode}, steps: {steps}, loss: {}, q_mean: {}, td_error: {}, eps: {}", loss.into_scalar::<f32>(), q_mean.into_scalar::<f32>(), td_error.mean().into_scalar::<f32>(), policy.eps()); }
+    }
+}
